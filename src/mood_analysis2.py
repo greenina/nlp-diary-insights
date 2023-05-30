34f13fd425c3ccdb59fb76663e1c4042b99d5157
greenina <igreen0485@gmail.com>
2023-05-31 01:23:04 +0900

    Add historical events

--- src/mood_analysis2.py
+++ src/mood_analysis2.py
@@ -69,7 +69,10 @@ df['diary_contents'] = df['diary_contents'].apply(lambda x: x.split())
 def get_emotion_count(words):
     words_in_lexicon = nrc_lexicon[nrc_lexicon['word'].isin(words)]
 
-    emotion_count = words_in_lexicon.drop('word', axis=1).sum().to_dict()
+    total_words = len(words)
+
+    emotion_count = (words_in_lexicon.drop(
+        'word', axis=1).sum() / total_words).to_dict()
 
     return emotion_count
 
@@ -109,3 +112,31 @@ for pair, count in most_common_pairs:
     percentage = count / total_pairs * 100
     print(
         f"When one feels {pair[0]}, it's {percentage:.2f}% likely that one will feel {pair[1]} the next day.")
+
+historical_events = {
+    "1942-06-12": "Anne Frank's 13th birthday",
+    "1942-07-06": "Anne Frank and her family go into hiding in the Secret Annex in Amsterdam, Netherlands",
+    "1942-08-01": "Anne starts writing about other Jews hiding with her family in the Secret Annex",
+    "1944-08-04": "The Secret Annex is discovered by the Nazis, Anne Frank and her family are arrested",
+    "1942-01-20": "Wannsee Conference: The Nazis decide on the 'Final Solution' for Europe's Jews",
+    "1942-06-01": "Operation Blau: The German army attacks the Soviet Union, capturing several cities including Stalingrad",
+    "1943-04-19": "Warsaw Ghetto Uprising: Jews in the Warsaw Ghetto rise up against the Nazis",
+    "1944-06-06": "D-Day: The Allied forces land in Normandy, France, beginning the end of Nazi Germany",
+    "1944-07-20": "Operation Valkyrie: Failed attempt to assassinate Hitler within the German army"
+}
+
+df['historical_event'] = df['written_date'].map(historical_events)
+
+
+for event_date, event_description in historical_events.items():
+    event_date = pd.to_datetime(event_date)
+    start_date = event_date - pd.Timedelta(days=7)
+    end_date = event_date + pd.Timedelta(days=7)
+    df_event = df[(df['written_date'] >= start_date)
+                  & (df['written_date'] <= end_date)]
+    plt.figure(figsize=(10, 5))
+    plt.plot(df_event['written_date'], df_event['compound_score'])
+    plt.title(event_description)
+    plt.xlabel('Date')
+    plt.ylabel('Sentiment Score')
+    plt.show()
